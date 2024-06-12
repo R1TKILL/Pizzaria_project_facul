724e302e58e1a8ba5d75f0891fef95e29e69e8d5
R1TKILL <antoniojunio402@gmai.com>
2024-06-12 16:19:17 -0300

    New Updates

---
+++ pizzas.js
@@ -0,0 +1,159 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/pizza1.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/pizza2.png',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/pizza3.png',
+        price: [23.00, 26.00, 28.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+      },
+      {
+        id: 4,
+        name: 'Brasileira',
+        img: 'images/pizza4.png',
+        price: [25.00, 28.00, 30.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+      },
+      {
+        id: 5,
+        name: 'Portuguesa',
+        img: 'images/pizza5.png',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+      },
+      {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/pizza6.png',
+        price: [30.00, 33.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+      },
+      {
+        id: 7,
+        name: 'Banana com canela',
+        img: 'images/pizza7.png',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Mussarela, banana, canela e açúcar'
+      },
+      {
+        id: 8,
+        name: 'Chocolate com morango',
+        img: 'images/pizza8.png',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      },
+      // Paraenses.
+      {
+        id: 9,
+        name: 'pizza de camarão com açai.',
+        img: 'images/pizza_açai.jpeg',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Camarão rosa, queijo, açai e azeitonas'
+      },
+      {
+        id: 10,
+        name: 'pizza de camarão com jambu.',
+        img: 'images/pizza_camarao_com_jambu.jpg',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Camarão, jambu e queijo'
+      },
+      {
+        id: 11,
+        name: 'pizza de pato no tucupi.',
+        img: 'images/pizza_pato_tucupi.jpg',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Carne de pato e tucupi'
+      },
+      {
+        id: 12,
+        name: 'pizza de pirarucu com jambu e tucupi.',
+        img: 'images/pizza_pirarucu.jpeg',
+        price: [25.00, 28.00, 30.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Peixe pirarucu, jambu e tucupi'
+      },
+      {
+        id: 13,
+        name: 'pizza de file de filhote.',
+        img: 'images/pizza_file_filhote.jpg',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'filé de filhote, com molho quatro queijos, castanha do Pará, tucupi e jambú'
+      },
+];
